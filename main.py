6bce673ade7c36b9c461435b9a6ed248f02c9954
ellanaqira <ellanrepo@gmail.com>
2026-04-16 23:02:55 +0800

    Refactor comments in CPU info functions for clarity and consistency

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ def vendor():
                 return(line.split(":")[1].strip())
 
 def proc_name():
-    # return processor model name
+    # return processor name
     with open('/proc/cpuinfo', 'r') as file:
         for line in file:
             if line.startswith('model name'):
                 return(line.split(":")[1].strip())
             
 def cpu_family():
-    # return processor model name
+    # return cpu family value
     with open('/proc/cpuinfo', 'r') as file:
         for line in file:
             if line.startswith('cpu family'):
                 return(line.split(":")[1].strip())
             
 def cpu_model():
-    # return processor model name
+    # return cpu model value
     with open('/proc/cpuinfo', 'r') as file:
         for line in file:
             if line.startswith('model'):
