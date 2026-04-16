5212ad26089e93e971600cd785f91029d0fd304d
ellanaqira <ellanrepo@gmail.com>
2026-04-16 22:35:54 +0800

    - Fixed the 'proc name' function to avoid bugs and ambiguity - Added the 'vendor' function to return the processor vendor name - Added the 'cpu_family' function to return the CPU family value - Added the 'cpu_model' function to return the CPU model value

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-import subprocess
+def vendor():
+    # return processor vendor name
+    with open('/proc/cpuinfo', 'r') as file:
+        for line in file:
+            if line.startswith('vendor'):
+                return(line.split(":")[1].strip())
 
 def proc_name():
-    # return processor name
-    cmd = "lscpu | grep 'Model name'"
-    raw_proc_name = subprocess.run(cmd, capture_output=True, text=True, shell=True).stdout
-    proc_name = raw_proc_name.split(":")[1].strip()
-    return proc_name
+    # return processor model name
+    with open('/proc/cpuinfo', 'r') as file:
+        for line in file:
+            if line.startswith('model name'):
+                return(line.split(":")[1].strip())
+            
+def cpu_family():
+    # return processor model name
+    with open('/proc/cpuinfo', 'r') as file:
+        for line in file:
+            if line.startswith('cpu family'):
+                return(line.split(":")[1].strip())
+            
+def cpu_model():
+    # return processor model name
+    with open('/proc/cpuinfo', 'r') as file:
+        for line in file:
+            if line.startswith('model'):
+                return(line.split(":")[1].strip())
+            
