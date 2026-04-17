eca82df2deeef6830ac8377f388edd08165a58e2
ellanaqira <ellanrepo@gmail.com>
2026-04-17 21:57:41 +0800

    refactor: change function name that already made before from 'vendor' to 'vendorid' feat: add 'vendor' function to return processor vendors name feat: add  'stepping' function to return cpu stepping value feat: add 'speed' funtion to return thread speed in MHz by the number of thread

--- sypeek/cpu.py
+++ sypeek/cpu.py
@@ -1,9 +1,24 @@
-def vendor():
-    # return processor vendor name
+def vendorid():
+    # return processor vendors id name
     with open('/proc/cpuinfo', 'r') as file:
         for line in file:
             if line.startswith('vendor_id'):
                 return(line.split(":")[1].strip())
+            
+def vendor():
+    # return processor vendors name
+    id_ext = ["Authentic", "CPU", "Driven", "Geode by NSC", "Genuine", "isbetter!", "RiseRise", "Sis Sis", "UMC UMC", "VIA VIA"]
+    with open('/proc/cpuinfo', 'r') as file:
+        for line in file:
+            if line.startswith('vendor_id'):
+                line = line.split(':')[1].strip()
+                for i in id_ext:
+                    if i in line:
+                        line = line.replace(i,'')
+                    else:
+                        line
+                return line
+print(vendor())   
 
 def name():
     # return processor name
@@ -41,3 +56,23 @@ def model():
         for line in file:
             if line.startswith('model'):
                 return(line.split(":")[1].strip())
+            
+def stepping():
+    # return cpu stepping value
+    with open('/proc/cpuinfo', 'r') as file:
+        for line in file:
+            if line.startswith('stepping'):
+                return(line.split(':')[1].strip())
+            
+def speed(threads_num):
+    # thread_num for number of threads
+    # get threads speed in MHz by the number
+    if threads_num < 0 or threads_num > (threads()-1):
+        return f"not included: thread number ({threads_num}) does not match the number of threads, which is (0-{threads()-1})"
+    else:
+        with open('/proc/cpuinfo', 'r') as file:
+            for line in file:
+                if line.startswith(f"processor	: {threads_num}"):
+                    for line in file:
+                        if line.startswith("cpu MHz"):
+                            return(line.split(':')[1].strip())
