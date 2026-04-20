5e57126a20753dd1fb45bc4cc75bad7a517679c6
ellanaqira <ellanrepo@gmail.com>
2026-04-20 10:56:11 +0800

    refactor: replace file reading with subprocess calls for CPU information retrieval

--- sypeek/cpu.py
+++ sypeek/cpu.py
@@ -2,58 +2,39 @@ import subprocess
 
 def vendorid():
     # return processor vendors id name
-    with open('/proc/cpuinfo', 'r') as file:
-        for line in file:
-            if line.startswith('vendor_id'):
-                return(line.split(":")[1].strip())
-            
+    cpu_venid = subprocess.run("lscpu | grep 'Vendor ID'", shell=True, capture_output=True, text=True).stdout
+    return cpu_venid.split(':')[1].strip()
+         
 def vendor():
     # return processor vendors name
-    # processor manufacturer ID string
-    id_ext = ["Authentic", "CPU", "Driven", "Geode by NSC", "Genuine", "isbetter!", "RiseRise", "Sis Sis", "UMC UMC", "VIA VIA"]
-    with open('/proc/cpuinfo', 'r') as file:
-        for line in file:
-            if line.startswith('vendor_id'):
-                line = line.split(':')[1].strip()
-                for i in id_ext:
-                    if i in line:
-                        line = line.replace(i,'')
-                    else:
-                        line
-                return line 
+    id_ext = ["Authentic", "CPU", "Driven", "Geode by NSC", "Genuine", "isbetter!", "RiseRise", "Sis Sis", "UMC UMC", "VIA VIA"] # processor manufacturer ID string
+    cpu_ven = subprocess.run("lscpu | grep 'Vendor ID'", shell=True, capture_output=True, text=True).stdout
+    cpu_ven = cpu_ven.split(':')[1].strip()
+    for ext in id_ext:
+        if ext in cpu_ven:
+            return cpu_ven.replace(ext, '')
 
 def name():
-    # return processor name
-    with open('/proc/cpuinfo', 'r') as file:
-        for line in file:
-            if line.startswith('model name'):
-                return (line.split(":")[1].strip())
+    # return cpu model name
+    model_name = subprocess.run("lscpu | grep 'Model name'", shell=True, capture_output=True, text=True).stdout
+    return model_name.split(':')[1].strip()          
             
 def cores():
     # return number of cpu cores
-    with open('/proc/cpuinfo', 'r') as file:
-        for line in file:
-            if line.startswith('cpu cores'):
-                return int(line.split(':')[1].strip())
-   
+    core = subprocess.run("lscpu | grep 'Core'", shell=True, capture_output=True, text=True).stdout
+    return int(core.split(':')[1].strip())
+
 def threads():
     # return number of threads
-    thread_num = 0
-    with open('/proc/cpuinfo', 'r') as file:
-        for line in file:
-            if line.startswith('processor'):
-                thread_num += 1
-    return(thread_num)
+    thread = subprocess.run("lscpu | grep 'Thread'", shell=True, capture_output=True, text=True).stdout
+    thread = int(thread.split(':')[1].strip())
+    return thread * cores()
 
 def family():
     # return cpu family
-    result = subprocess.run("cpuid", capture_output=True, text=True).stdout
-    result = result.split('\n')
-    for k in result:
-        if "family" in k:
-            k = k.split('=')[1].strip()
-            return k
-    
+    fam = subprocess.run("cpuid | grep -m1 'family'", shell=True, capture_output=True, text=True).stdout
+    return fam.split('=')[1].strip()
+print(family())
 def family_synth():
     # return cpu family synth
     result = subprocess.run("cpuid", capture_output=True, text=True).stdout
