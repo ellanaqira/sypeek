392fbaca0fae201de6a0bd1190aaf75471cda93e
ellanaqira <ellanrepo@gmail.com>
2026-04-24 11:18:36 +0800

    refactor: replaced "free" with "pro | grep 'Mem'" and removed the if statement to optimize the code and eliminate ambiguity.

--- sypeek/memory.py
+++ sypeek/memory.py
@@ -1,39 +1,26 @@
 import subprocess
 
 def total():
-    # return total memory
-    result = subprocess.run("free", capture_output=True, text=True).stdout
-    result = result.split('\n')
-    for k in result:
-        if "Mem" in k:
-            k = float(k.split()[1].strip()) / 1000000
-            return f"{k:.2f}"
+    # return total memory in Gigabytes
+    result = subprocess.run("free | grep 'Mem'", shell=True ,capture_output=True, text=True).stdout
+    result = float(result.split()[1].strip()) / 1000000
+    return result
 
 def used():
-    # return used memory
-    result = subprocess.run("free", capture_output=True, text=True).stdout
-    result = result.split('\n')
-    for k in result:
-        if "Mem" in k:
-            k = float(k.split()[2].strip()) / 1000000
-            return f"{k:.2f}"  
+    # return used memory in Gigabytes
+    result = subprocess.run("free | grep 'Mem'", shell=True, capture_output=True, text=True).stdout
+    result = float(result.split()[2].strip()) / 1000000
+    return result
         
-def available():
-    # return available memory
-    result = subprocess.run("free", capture_output=True, text=True).stdout
-    result = result.split('\n')
-    for k in result:
-        if "Mem" in k:
-            k =  float(k.split()[6].strip()) / 1000000
-            return f"{k:.2f}"
-
 def free():
     # return free memory
-    result = subprocess.run("free", capture_output=True, text=True).stdout
-    result = result.split('\n')
-    for k in result:
-        if "Mem" in k:
-            k = float(k.split()[3].strip()) / 1000000
-            return f"{k:.2f}"
+    result = subprocess.run("free | grep 'Mem'", shell=True, capture_output=True, text=True).stdout
+    result = float(result.split()[3].strip()) / 1000000
+    return result
 
+def available():
+    # return available memory
+    result = subprocess.run("free | grep 'Mem'", shell=True, capture_output=True, text=True).stdout
+    result = float(result.split()[6].strip()) / 1000000
+    return result
             
