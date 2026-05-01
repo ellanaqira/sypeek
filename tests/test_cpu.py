802286a46fe5b42fdc2a8146935f5554b4be7266
ellanaqira <ellanrepo@gmail.com>
2026-05-01 11:38:18 +0800

    test: enhance validation for cpu.cores, cpu.speed, and cpu.temp functions

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -20,6 +20,7 @@ def test_cpu_cores():
     assert cpu.cores('p') == 4 # physical core(s)
     assert cpu.cores('P') == 4
 
+    # error output
     with pytest.raises(ValueError, match="core must be 'l' or 'p'"):
         cpu.cores('q')
 
@@ -44,3 +45,50 @@ def test_cpu_model_synth():
 
 def test_cpu_stepping():
     assert cpu.stepping() == 1
+
+
+def test_cpu_speed():
+    assert cpu.speed(0)
+    assert cpu.speed(1)
+    assert cpu.speed(2)
+    assert cpu.speed(3)
+    assert cpu.speed(4)
+    assert cpu.speed(5)
+    assert cpu.speed(6)
+    assert cpu.speed(7)
+
+    # error output
+    with pytest.raises(ValueError, match="core number must be between 0 and 7"):
+        cpu.speed(8)
+
+    with pytest.raises(ValueError, match="core number must be between 0 and 7"):
+        cpu.speed(3.0)
+
+    with pytest.raises(ValueError, match="core number must be between 0 and 7"):
+        cpu.speed('0')
+
+    with pytest.raises(ValueError, match="core number must be between 0 and 7"):
+        cpu.speed(True)
+
+
+def test_cpu_temperature():
+    assert cpu.temp('c')
+    assert cpu.temp('C')
+    assert cpu.temp('f')
+    assert cpu.temp('F')
+    assert cpu.temp('k')
+    assert cpu.temp('K')
+
+    #error output
+    with pytest.raises(ValueError, match="temperature scale must be 'c', 'f', or 'k'"):
+        cpu.temp('x')
+
+    with pytest.raises(ValueError, match="temperature scale must be 'c', 'f', or 'k'"):
+        cpu.temp(2)
+
+    with pytest.raises(ValueError, match="temperature scale must be 'c', 'f', or 'k'"):
+        cpu.temp(2.0)
+
+    with pytest.raises(ValueError, match="temperature scale must be 'c', 'f', or 'k'"):
+        cpu.temp(True)
+
