2dce2b30f6b30083cd6c026b8fee7db1aacbbcd5
ellanaqira <ellanrepo@gmail.com>
2026-05-04 13:45:32 +0800

    refactor: streamline memory data retrieval and enhance error handling

--- sypeek/memory.py
+++ sypeek/memory.py
@@ -1,76 +1,90 @@
-import subprocess
 
+_GENERAL_MEMO_ERROR_MESSAGE = "Sorry, something went wrong, couldn't get data from memory"
 
-_MEMO_GENERAL_ERROR_MESSAGE = "something went wrong, couldn't get data from memory"
-
-
-def _get_data(about: str, keyword: str):
-    data_list = []
+def _get_memo_datas(keyword: str):
     data_dict = {}
-
     try:
-        data = subprocess.run("free", capture_output=True, text=True)
-
+        with open("/proc/meminfo") as f:
+            for line in f:
+                key, value = [x.strip().replace("kB", '').replace("kb", '') for x in line.split(':', 1)]
+                data_dict[key] = value
+                
     except FileNotFoundError:
-        return _MEMO_GENERAL_ERROR_MESSAGE
+        return _GENERAL_MEMO_ERROR_MESSAGE
     
     else:
-        data = data.stdout.splitlines()
-        for line in data:
-            line = line.split()
-            data_list.append(line)
-
-        if about == "memo":
-            del data_list[1][0], data_list[2]
-            
-        elif about == "swap":
-            del data_list[1], data_list[1][0]
-
-        for key, value in zip(data_list[0], data_list[1]):
-            data_dict[key] = value
-
-        try:
-            # return value in kibibytes - 1 kibibyte (KiB) is 1024 bytes.    
-            return int(data_dict[keyword])
-        
-        except ValueError:
-            return _MEMO_GENERAL_ERROR_MESSAGE
-        
-        except KeyError:
-            return _MEMO_GENERAL_ERROR_MESSAGE
+        # return value in Kilobyte
+        return int(data_dict[keyword])
    
 
+class _Return_Data:
+    def __init__(self, keyword: str):
+        self.keyword = keyword
+    
+    def _get_memo_data(self):
+        try:
+            return _get_memo_datas(self.keyword)
+        except KeyError:
+            return _GENERAL_MEMO_ERROR_MESSAGE
 
-# Memory ==============================================
+
+# General Memory ===============================================
 
 def mem_total():
     # return total memory
-    return _get_data("memo", "total")
-
-def mem_used():
-    # return used memory
-    return _get_data("memo", "used")
+    total_memo = _Return_Data("MemTotal")
+    return total_memo._get_memo_data()
 
 def mem_free():
     # return free memory
-    return _get_data("memo", "free")
+    free_memo = _Return_Data("MemFree")
+    return free_memo._get_memo_data()
 
 def mem_available():
     # return available memory
-    return _get_data("memo", "available")
+    available_memo = _Return_Data("MemAvailable")
+    return available_memo._get_memo_data()
 
 
-# Swap Memory =========================================
 
-def swap_mem_total():
-    # return total swap memory
-    return _get_data("swap", "total")
+# Active Memory ================================================
 
-def swap_mem_used():
-    # return used swap memory
-    return _get_data("swap", "used")
+def mem_active():
+    # return active memory recently
+    active_memo = _Return_Data("Active")
+    return active_memo._get_memo_data()
 
-def swap_mem_free():
-    # return free swap memory
-    return _get_data("swap", "free")
-            
+def mem_inactive():
+    # return inactive memory
+    incative_memo = _Return_Data("Inactive")
+    return incative_memo._get_memo_data()
+
+"""
+The (anon) means that this amount of memory
+is being used by a service or application.
+"""
+
+def mem_active_anon():
+    # return active memory (anon)
+    active_memo_anon = _Return_Data("Active(anon)")
+    return active_memo_anon._get_memo_data()
+
+def mem_inactive_anon():
+    # return active memory (anon)
+    inactive_memo_anon = _Return_Data("Inactive(anon)")
+    return inactive_memo_anon._get_memo_data()
+
+"""
+The (file) means that the amount
+of memory is being used by cache.
+"""
+
+def mem_active_file():
+    # return active memory (file)
+    active_memo_file = _Return_Data("Active(file)")
+    return active_memo_file._get_memo_data()
+
+def mem_inactive_file():
+    # return inactive memory (file)
+    inactive_memo_file = _Return_Data("Inactive(file)")
+    return inactive_memo_file._get_memo_data()
