d904fed200590f13755e1ede08a91026d6d7abc3
ellanaqira <ellanrepo@gmail.com>
2026-05-12 00:16:07 +0800

    refactor: update memory import and fix references in test cases

--- tests/test_memo.py
+++ tests/test_memo.py
@@ -1,5 +1,5 @@
 import pytest
-from sypeek import memory as mem
+from sypeek import memory
 
 
 class _Return_Mem_Exception:
@@ -7,13 +7,13 @@ class _Return_Mem_Exception:
         self.keyword_error = keyword_error
 
     def _return_mem_meminfo_exeption(self):
-        with pytest.raises(mem.MemoInfoError) as excinfo:
-            mem._get_memo_data_meminfo("invalid_keyword", self.keyword_error)
+        with pytest.raises(memory.MemoInfoError) as excinfo:
+            memory._get_memo_data_meminfo("invalid_keyword", self.keyword_error)
         assert str(excinfo.value) == f"Couldn't get '{self.keyword_error}' information"
 
     def _return_mem_free_exeption(self):
-        with pytest.raises(mem.MemoInfoError) as excinfo:
-            mem._get_memo_data_free("invalid_keyword", self.keyword_error)
+        with pytest.raises(memory.MemoInfoError) as excinfo:
+            memory._get_memo_data_free("invalid_keyword", self.keyword_error)
         assert str(excinfo.value) == f"Couldn't get '{self.keyword_error}' information"
 
 
@@ -25,7 +25,7 @@ class _Mock_Function:
     def _mock_meminfo_func(self, mocker_plugin):
         mocked_value: int = 1999999.999
 
-        mock_meminfo_func = mocker_plugin.patch("sypeek.memory._Return_Data._show_meminfo_memo_data")
+        mock_meminfo_func = mocker_plugin.patch("sypeek.memory._get_memo_data_meminfo")
         mock_meminfo_func.return_value = mocked_value
 
         assert lambda:self.func_name == mocked_value
@@ -34,7 +34,7 @@ class _Mock_Function:
     def _mock_free_func(self, mocker_plugin):
         mocked_value: int = 1999999.999
 
-        mock_free_func = mocker_plugin.patch("sypeek.memory._Return_Data._show_free_memo_data")
+        mock_free_func = mocker_plugin.patch("sypeek.memory._get_memo_data_free")
         mock_free_func.return_value = mocked_value
 
         assert lambda:self.func_name == mocked_value
