64896da1b07fa4645dab95f1393b2db0cce374f4
ellanaqira <ellanrepo@gmail.com>
2026-05-15 17:20:14 +0800

    refactor: simplify board information retrieval by removing class structure

--- sypeek/board.py
+++ sypeek/board.py
@@ -1,23 +1,18 @@
 
-class _Get_Board_Info:
-    def __init__(self, file_name: str, keyword_error: str):
-        self.file_name = file_name
-        self.keyw_error = keyword_error
+def _get_board_info(file_name: str, keyword_error: str):
+    try:
+        with open(f"/sys/class/dmi/id/{file_name}") as f:
+            return (f.read().strip())
 
-    def _get_board_info(self):
-        try:
-            with open(f"/sys/class/dmi/id/{self.file_name}") as f:
-                return (f.read().strip())
-
-        except FileNotFoundError:
-            return f"Couldn't get '{self.keyw_error}' information"
-
+    except FileNotFoundError:
+        return f"Couldn't get '{keyword_error}' information"
+    
 
 def board_name():
-    return _Get_Board_Info("board_name", "Board Name")._get_board_info()
+    return _get_board_info("board_name", "Board Name")
 
 def board_vendor():
-    return _Get_Board_Info("board_vendor", "Board Vendor")._get_board_info()
+    return _get_board_info("board_vendor", "Board Vendor")
 
 def board_version():
-    return _Get_Board_Info("board_version", "Board Version")._get_board_info()
+    return _get_board_info("board_version", "Board Version")
