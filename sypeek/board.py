17eb7728cfc997ba7688eef512f8dcfe681b8d3f
ellanaqira <ellanrepo@gmail.com>
2026-06-25 09:24:18 +0800

    feat: change the way board.py returning data 	create get_brddt function to return board data by keyword 	or flags(specific keyword) and add _VerticalList class to 	return formated output

--- sypeek/board.py
+++ sypeek/board.py
@@ -1,3 +1,5 @@
+import os
+
 class BoardInfoError(Exception):
     """
     Exception raised and displays an error message
@@ -13,20 +15,70 @@ class BoardInfoError(Exception):
         super().__init__(self.message)
 
 
-def _get_board_info(file_name: str, keyword_error: str):
-    try:
-        with open(f"/sys/class/dmi/id/{file_name}") as f:
-            return (f.read().strip())
+class _VerticalList:
+    """
+    Return "vertical list" from the raw list.
 
-    except FileNotFoundError:
-        raise BoardInfoError(f"Couldn't get '{keyword_error}' information")
+    Atributes:
+    * data_list = raw list that want to turn into "vertical list"
+    """
+    def __init__(self, data_list):
+        self.data_list = data_list
+
+
+    def __str__(self):
+        longest_key: int = 0
+        for data in self.data_list:
+            for key in data.keys():
+                if len(key) > longest_key:
+                    longest_key = len(key)
+
+        organize = []
+        for data in self.data_list:
+            for key,value in data.items():
+                organize.append(f"{key}{" "*(longest_key-len(key))} : {value}")
+            organize.append("\n")
+
+        return "\n".join(organize)
+
+
+
+def get_brddt(keyword: str):
+    """
+    brddt means BoaRD DaTa, this funtion is used to
+    get information about board by a keyword.
     
+    Parameters:
+    * keyword: str = keyword to get related data
+    * flags (special keyword: str):
+        - "--all" : return sorted list of data
+        - "--raw" : return data in form of raw dictionary
+        - "--len" : returns the number of data  
+    """
+    board_data = {}
+    p: str = "/sys/class/dmi/id/"
+    try:
+        for plnf in os.scandir(p):
+            if plnf.is_file():
+                try:
+                    with open(plnf.path, "r") as f:
+                        board_data[os.path.basename(plnf)] = (f.read()).strip()
+                except PermissionError:
+                    None 
+        try:
+            if keyword == "--all":
+                return _VerticalList([board_data])
+            
+            if keyword == "--raw":
+                return board_data
+            
+            if keyword == "--len":
+                return int(len(board_data))
 
-def board_name():
-    return _get_board_info("board_name", "Board Name")
-
-def board_vendor():
-    return _get_board_info("board_vendor", "Board Vendor")
-
-def board_version():
-    return _get_board_info("board_version", "Board Version")
+            return board_data[keyword]
+        except KeyError:
+            raise BoardInfoError(f"the data of '{keyword}' not available")
+        
+    except FileNotFoundError:
+        raise BoardInfoError("failed to get board data")
+    
