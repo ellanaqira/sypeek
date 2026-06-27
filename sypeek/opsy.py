e20d83e660d83d0e8bab1d7ec22601b668fb918b
ellanaqira <ellanrepo@gmail.com>
2026-06-27 20:35:58 +0800

    feat: import verlist module and change the way opsy.py collecting and presenting data

--- sypeek/opsy.py
+++ sypeek/opsy.py
@@ -1,3 +1,6 @@
+from _helper.verlist import _VerticalList as _VerList
+
+
 class OpSyInfoError(Exception):
     """
     Exception raised and displays an error message
@@ -13,13 +16,38 @@ class OpSyInfoError(Exception):
         super().__init__(self.message)
 
 
-def _raise_opsyinfoerror(keyword_error: str):
-    raise OpSyInfoError(f"Couldn't get '{keyword_error}' information")
+class _ReadSF:
+    """
+    Read file and return it as a string.
 
+    Attributes:
+    * file_path -- the relative file path of destination file in string
+    """
+    def __init__(self, file_path: str):
+        self.fpath = file_path
 
-def _get_os_info(keyword: str, keyword_error: str):
-    error_msg: str = f"Couldn't get '{keyword_error}' information"
+    def returnstr(self):
+        try:
+            with open(self.fpath) as f:
+                result = f.read()
+        except FileNotFoundError:
+            return "failed to get operating system data (!)" 
+        else:
+            return result.strip()
+    
 
+def get_osdt(keyword: str):
+    """
+    Osdt means Operating System DaTa, get information about
+    operating system by a keyword.
+    
+    Parameters:
+    * keyword: str = keyword to get related data
+    * flags (special keyword: str):
+        - "--all" : return sorted list of data
+        - "--raw" : return data in form of raw dictionary
+        - "--len" : returns the number of data  
+    """
     os_info_list: list = []
     os_info_dict: dict = {}
 
@@ -36,50 +64,52 @@ def _get_os_info(keyword: str, keyword_error: str):
             value = str(data[1]).replace('"', '')
             os_info_dict[key] = value
 
+        ver = _ReadSF("/proc/version")
+        os_info_dict["FULL_VERSION"] = ver.returnstr()
+
+        ver_sig = _ReadSF("/proc/version_signature")
+        os_info_dict["VERSION_SIGNATURE"] = ver_sig.returnstr()
+
+        host_name = _ReadSF("/etc/hostname")
+        os_info_dict["HOST_NAME"] = host_name.returnstr()
+
         try:
+            if keyword == "--all":
+                return _VerList([os_info_dict])
+            if keyword == "--raw":
+                return os_info_dict
+            if keyword == "--len":
+                return len(os_info_dict)
+            
             return os_info_dict[keyword]
+        
         except KeyError:
-            raise OpSyInfoError(error_msg)
+            raise OpSyInfoError(f"the data of '{keyword}' not available")
 
     except FileNotFoundError:
-        raise OpSyInfoError(error_msg)
-    
-
-    
-def distro_name():
-    return _get_os_info("NAME", "Distribution Name")
-
-def distro_ver():
-    return _get_os_info("VERSION_ID", "Distribution Version")
+        raise OpSyInfoError("failed to get operating system data (!)")
 
 
 
-def opsy_kernel():
-    try:
-        with open("/proc/version") as f:
-            f = f.read().split('(')
-    except FileNotFoundError:
-        _raise_opsyinfoerror("Kernel Name")   
-    else:
-        return f[0].strip()
-    
-def host_name():
-    try:
-        with open("/etc/hostname") as f:
-            f = f.read().strip()
-    except FileNotFoundError:
-        _raise_opsyinfoerror("Host Name")
-    else:
-        return f
-    
-def up_time(appearance: str = ""):
+def get_uptime(appearance: str = ""):
+    """
+    Returns the value of how long the device was active,
+    at default this function return in minutes.
+
+    Parameter:  
+    * appearance: str
+        you can leave it blank or insert "p"/"P" to
+        return the time value in "pretty" or human
+        readable format
+    """
+
     try:
         with open("/proc/uptime") as time:
             time = time.read().split()
             time =int(float(time[0]) / 60)
             
     except FileNotFoundError:
-        _raise_opsyinfoerror("Uptime")
+        raise OpSyInfoError(f"the data of 'Uptime' not available")
 
     else:
         if appearance.lower() == 'p':
@@ -98,4 +128,4 @@ def up_time(appearance: str = ""):
             return int(time)
         
         else:
-            return "appearances most be 'p' or an empty string"
+            return "appearances most be 'p' or an empty string"  
