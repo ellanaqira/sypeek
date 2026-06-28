25b7c48d4988231fdf2bc3cc54607245c3d1e6b5
ellanaqira <ellanrepo@gmail.com>
2026-06-28 12:14:46 +0800

    feat: initialize _verlist module and import function inside other module 	change import * to import <function_name> to be more specific

--- sypeek/__init__.py
+++ sypeek/__init__.py
@@ -1,5 +1,5 @@
-from .bios import *
-from .board import *
-from .cpu import *
-from .memory import *
-from .opsy import *
+from .board import get_brddt
+from .cpu import get_cpudt_sttc, get_cpudt_dynmc, get_cpudt_snsr
+from .memory import get_memdt
+from .opsy import get_osdt, get_uptime
+from ._verlist import _VerticalList
