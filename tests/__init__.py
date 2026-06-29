56f4d17c1941bf5d0f83422c59d527ff07238826
ellanaqira <ellanrepo@gmail.com>
2026-06-29 23:26:36 +0800

    refactor: change import all from module to import function from module

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
-from sypeek.bios import *
-from sypeek.board import *
-from sypeek.cpu import *
-from sypeek.memory import *
-from sypeek.opsy import *
+from sypeek.board import get_brddt
+from sypeek.cpu import get_cpudt_sttc, get_cpudt_dynmc, get_cpudt_snsr
+from sypeek.memory import get_memdt
+from sypeek.opsy import get_osdt, get_uptime
+from sypeek._verlist import _VerticalList
