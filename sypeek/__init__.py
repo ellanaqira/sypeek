6c12970f50bf08e95bf9db9f7eef737786d3dcc7
ellanaqira <ellanrepo@gmail.com>
2026-06-30 14:58:38 +0800

    fix: add . in front of module name to indicate relative import

--- sypeek/__init__.py
+++ sypeek/__init__.py
@@ -1,5 +1,5 @@
-from board import get_brddt
-from cpu import get_cpudt_sttc, get_cpudt_dynmc, get_cpudt_snsr
-from memory import get_memdt
-from opsy import get_osdt, get_uptime
-from _verlist import _VerticalList
+from .board import get_brddt
+from .cpu import get_cpudt_sttc, get_cpudt_dynmc, get_cpudt_snsr
+from .memory import get_memdt
+from .opsy import get_osdt, get_uptime
+from ._verlist import _VerticalList
